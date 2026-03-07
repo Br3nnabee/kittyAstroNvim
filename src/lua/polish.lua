74cee19fb00565e1b16d4a31352bbc242feb1424
Br3nnabee <contact@br3nnabee.dev>
2026-03-07 15:19:36 +0100

    Zellij integration

--- src/lua/polish.lua
+++ src/lua/polish.lua
@@ -3,3 +3,18 @@
 -- fit in the normal config locations above can go here
 
 vim.cmd ":TransparentEnable"
+
+-- Zellij Integration: Hide UI on enter, restore on leave
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    os.execute("zellij action switch-layout plain")
+    os.execute("zellij action switch-mode locked")
+  end,
+})
+
+vim.api.nvim_create_autocmd("VimLeave", {
+  callback = function()
+    os.execute("zellij action switch-mode normal")
+    os.execute("zellij action switch-layout pills")
+  end,
+})
